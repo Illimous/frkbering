5a30400ab6b2deda47b95314121287d2886f2908
Line Bering <nordictallows@gmail.com>
2025-02-07 20:03:47 +0100

    Opdater Prisma konfiguration og build process

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../prisma/generated/client';
 import { sql } from '@vercel/postgres';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
