bc1e50dd372c0c793bc7a2607878f509ac782269
Line Bering <nordictallows@gmail.com>
2025-02-07 20:17:48 +0100

    Ret Prisma klient import tilbage til standard

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/generated/client';
+import { PrismaClient } from '@prisma/client';
 import { sql } from '@vercel/postgres';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
